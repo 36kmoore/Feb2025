b1515bdabfd7015d431e3ae566b8ff335e2aeb1c
Kristen Moore <kristenmoore117@gmail.com>
2025-02-11 16:36:41 -0600

    Adding and refining Lab 10
    Refining some things on lab 10

--- Feb2025_1/src/Pack1/Lab1_10.java
+++ Feb2025_1/src/Pack1/Lab1_10.java
@@ -7,7 +7,11 @@ public class Lab1_10 {
 	static boolean is_pos_str (String str) {
 		int str_len = str.length();
 		for (int i = 1; i < str_len; i++) {
+			str.replaceAll(" ", "");
 			if(str.charAt(i) < str.charAt(i-1)) {
+				return false;
+			}
+			if(str.isEmpty()) {
 				return false;
 			}
 		}
@@ -23,7 +27,7 @@ public class Lab1_10 {
 		if (is_pos_str(str)) {
 			System.out.println("The letters " + "\"" + str + "\"" + " are a positive String");
 		} else {
-			System.out.println("The letters " + str + " are NOT a positive String");
+			System.out.println("The letters " + "\"" + str + "\"" + " are NOT a positive String");
 		}
 	}
 
